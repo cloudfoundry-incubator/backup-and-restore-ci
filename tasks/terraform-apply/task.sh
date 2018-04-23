1ea13169c8775a647d2a45916dfc75a718658c34
Chunyi Lyu <clyu@pivotal.io>
2018-04-23 15:36:30 +0100

    Add concourse task for terraform apply
    [#156993313]
    
    Signed-off-by: Rosie Bloxsom <rbloxsom@pivotal.io>

---
+++ tasks/terraform-apply/task.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x
+
+terraform version
+
+./bosh-backup-and-restore-meta/unlock-ci.sh
+
+git config --global user.name "PCF Backup & Restore CI"
+git config --global user.email "cf-lazarus@pivotal.io"
+
+pushd "bosh-backup-and-restore-meta/${TERRAFORM_DIR}"
+  set +x
+  terraform init
+  terraform apply -auto-approve
+  set -x
+
+  git add terraform.tfstate*
+  if git commit -m "Update terraform-state" ; then
+    echo "Update terraform-state"
+  else
+    echo "No deploy occurred; bailing out"
+  fi
+popd
+cp -r bosh-backup-and-restore-meta/ meta-with-updated-terraform/
