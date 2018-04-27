d907784b5e020ab0a1071de585387116f054dfe5
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-04-27 15:02:23 +0100

    Double-quote all the strings in terraform-apply task script
    [#156731185]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- tasks/terraform-apply/task.sh
+++ tasks/terraform-apply/task.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 set -u
 
 terraform version
 
 if [[ ! -z "$TERRFORM_STATE_PREPARE_CMD" ]]; then
-  $TERRAFORM_STATE_DIR/$TERRFORM_STATE_PREPARE_CMD
+  "$TERRAFORM_STATE_DIR/$TERRFORM_STATE_PREPARE_CMD"
 fi
 
-git config --global user.name $GIT_USER_NAME
-git config --global user.email $GIT_USER_EMAIL
+git config --global user.name "$GIT_USER_NAME"
+git config --global user.email "$GIT_USER_EMAIL"
 
 pushd "$TERRAFORM_STATE_DIR/$ENVIRONMENT_NAME"
   terraform init
@@ -25,4 +26,4 @@ pushd "$TERRAFORM_STATE_DIR/$ENVIRONMENT_NAME"
   fi
 popd
 
-cp -r $TERRAFORM_STATE_DIR $OUTPUT_TERRAFORM_STATE_DIR
+cp -r "$TERRAFORM_STATE_DIR" "$OUTPUT_TERRAFORM_STATE_DIR"
