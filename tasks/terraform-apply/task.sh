4f61fb23573586b6d78c28587689372ba037da60
Chunyi Lyu <clyu@pivotal.io>
2018-05-01 12:12:29 +0100

    Revert "Update TERRAFORM_STATE_DIR and TERRAFORM_STATE_PREPARE_CMD to have"
    This reverts commit 9904ddf034215a6134130e1af24545e6cd0a0615.

--- tasks/terraform-apply/task.sh
+++ tasks/terraform-apply/task.sh
@@ -6,23 +6,23 @@ set -u
 terraform version
 
 if [[ ! -z "$TERRAFORM_STATE_PREPARE_CMD" ]]; then
-  "$TERRAFORM_STATE_PREPARE_CMD"
+  "$TERRAFORM_STATE_DIR/$TERRAFORM_STATE_PREPARE_CMD"
 fi
 
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
-pushd "$TERRAFORM_STATE_DIR"
+pushd "$TERRAFORM_STATE_DIR/$ENVIRONMENT_NAME"
   terraform init
   terraform apply -auto-approve
 
   git add terraform.tfstate*
 
-  if git commit -m "Update terraform state"; then
-    echo "Updated terraform state"
+  if git commit -m "Update terraform-state for $ENVIRONMENT_NAME"; then
+    echo "Updated terraform-state for $ENVIRONMENT_NAME"
   else
-    echo "No change to terraform state"
+    echo "No change to terraform-state for $ENVIRONMENT_NAME"
   fi
 popd
 
-cp -r "$TERRAFORM_STATE_DIR/." "$OUTPUT_DIR"
+cp -r "$TERRAFORM_STATE_DIR/." "$OUTPUT_TERRAFORM_STATE_DIR"
