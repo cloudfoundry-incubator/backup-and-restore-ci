755f0732d5e408532ea9d6378f10d36fb5f8d80c
Chunyi Lyu <clyu@pivotal.io>
2018-05-01 12:12:38 +0100

    Revert "Update BOSH_STATE_DIR and BOSH_STATE_PREPARE_CMD to have complete path"
    This reverts commit b4bd2c98313b240006928ae3778af31a4b756f7b.

--- tasks/bosh-create-env/task.sh
+++ tasks/bosh-create-env/task.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 
 if [[ ! -z "$BOSH_STATE_PREPARE_CMD" ]]; then
-  "$BOSH_STATE_PREPARE_CMD"
+  "$BOSH_STATE_DIR/$BOSH_STATE_PREPARE_CMD"
 fi
 
 opsfiles_arguments=""
@@ -16,9 +16,9 @@ done
 pushd bosh-deployment
     # shellcheck disable=SC2086   # we need to expand $opsfiles_arguments
     bosh-cli create-env bosh.yml \
-    --state="../${BOSH_STATE_DIR}/bosh-state.json" \
-    --vars-store="../${BOSH_STATE_DIR}/creds.yml" \
-    --var-file gcp_credentials_json="../${BOSH_STATE_DIR}/gcp_service_account_key.json" \
+    --state="../${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}/bosh-state.json" \
+    --vars-store="../${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}/creds.yml" \
+    --var-file gcp_credentials_json="../${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}/gcp_service_account_key.json" \
     $opsfiles_arguments \
     -v director_name="$DIRECTOR_NAME" \
     -v internal_cidr=10.0.0.0/24 \
@@ -32,15 +32,14 @@ pushd bosh-deployment
     -v subnetwork="$SUBNET"
 popd
 
-pushd "${BOSH_STATE_DIR}"
+pushd "${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}"
   git add bosh-state.json
   git add creds.yml
-
-  if git commit -m "Update BOSH director state" ; then
-    echo "Updated BOSH director state"
+  if git commit -m "Update bosh state for $ENVIRONMENT_NAME" ; then
+    echo "Updated bosh-state for $ENVIRONMENT_NAME"
   else
-    echo "No change to BOSH director state"
+    echo "No change to BOSH state for $ENVIRONMENT_NAME"
   fi
 popd
 
-cp -r "${BOSH_STATE_DIR}/." "${OUTPUT_DIR}"
+cp -r "${BOSH_STATE_DIR}/." "${BOSH_STATE_OUTPUT_DIR}"
