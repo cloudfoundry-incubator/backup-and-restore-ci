38da6cec8d11f9199e64fb8b547c7b0912e90d56
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-05-10 14:46:31 +0100

    Add upload-b-drats-stemcell task
    [#156800775]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

---
+++ tasks/upload-b-drats-stemcell/task.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+if [[ ! -z "${TERRAFORM_STATE_PREPARE_CMD}" ]]; then
+  "terraform-state/${TERRAFORM_STATE_PREPARE_CMD}"
+fi
+
+if [[ ! -z "${BOSH_VARS_STORE_PREPARE_CMD}" ]]; then
+  "bosh-vars-store/${BOSH_VARS_STORE_PREPARE_CMD}"
+fi
+
+bosh_host="$(terraform output -state="terraform-state/${TERRAFORM_STATE_PATH}" director-ip)"
+bosh_ca_cert="$(bosh-cli int --path=/director_ssl/ca "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
+bosh_client_secret="$(bosh-cli int --path=/admin_password "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
+
+bosh_ca_cert_path="$(mktemp)"
+echo "${bosh_ca_cert}" > "${bosh_ca_cert_path}"
+
+bosh-cli --environment "${bosh_host}" \
+    --client "${BOSH_CLIENT}" \
+    --client-secret "${bosh_client_secret}" \
+    --ca-cert "${bosh_ca_cert_path}" \
+    upload-stemcell "${STEMCELL_URL}" -n
