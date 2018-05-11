4f8f638b52d5e95df5656986ce6dcc09af7e7edb
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-05-11 16:57:38 +0100

    extract-b-drats-integration-config now parse bosh creds
    [#156800775]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- tasks/extract-b-drats-integration-config/task.sh
+++ tasks/extract-b-drats-integration-config/task.sh
@@ -15,8 +15,11 @@ bosh_host="$(terraform output -state="${TERRAFORM_STATE_PATH}" director-ip)"
 bosh_ssh_username="${BOSH_SSH_USERNAME}"
 bosh_ssh_private_key="$(bosh-cli int --path=/jumpbox_ssh/private_key "${BOSH_VARS_STORE_PATH}")"
 timeout="${TIMEOUT_IN_MINUTES}"
+bosh_client="${BOSH_CLIENT}"
+bosh_client_secret="$(bosh-cli int --path=/admin_password "${BOSH_VARS_STORE_PATH}")"
+bosh_ca_cert="$(bosh-cli int --path=/director_ssl/ca "${BOSH_VARS_STORE_PATH}")"
 
-vars="bosh_host bosh_ssh_username bosh_ssh_private_key timeout"
+vars="bosh_host bosh_ssh_username bosh_ssh_private_key timeout bosh_client bosh_client_secret bosh_ca_cert"
 
 integration_config="{}"
 
