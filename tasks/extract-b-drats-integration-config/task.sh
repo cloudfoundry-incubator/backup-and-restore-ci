0191b2910c3c835776c09a18760c034381707b4c
Chunyi Lyu <clyu@pivotal.io>
2018-05-15 16:20:24 +0100

    Update timeout_in_minutes to be integer
    [#157563727]

--- tasks/extract-b-drats-integration-config/task.sh
+++ tasks/extract-b-drats-integration-config/task.sh
@@ -14,7 +14,7 @@ fi
 bosh_host="$(terraform output -state="${TERRAFORM_STATE_PATH}" director-ip)"
 bosh_ssh_username="${BOSH_SSH_USERNAME}"
 bosh_ssh_private_key="$(bosh-cli int --path=/jumpbox_ssh/private_key "${BOSH_VARS_STORE_PATH}")"
-timeout="${TIMEOUT_IN_MINUTES}"
+timeout_in_minutes="${TIMEOUT_IN_MINUTES}"
 bosh_client="${BOSH_CLIENT}"
 bosh_client_secret="$(bosh-cli int --path=/admin_password "${BOSH_VARS_STORE_PATH}")"
 bosh_ca_cert="$(bosh-cli int --path=/director_ssl/ca "${BOSH_VARS_STORE_PATH}")"
@@ -22,13 +22,13 @@ include_deployment_testcase="${INCLUDE_DEPLOYMENT_TESTCASE}"
 
 integration_config="{}"
 
-string_vars="bosh_host bosh_ssh_username bosh_ssh_private_key timeout bosh_client bosh_client_secret bosh_ca_cert"
+string_vars="bosh_host bosh_ssh_username bosh_ssh_private_key bosh_client bosh_client_secret bosh_ca_cert"
 for var in $string_vars
 do
   integration_config=$(echo ${integration_config} | jq ".${var}=\"${!var}\"")
 done
 
-other_vars="include_deployment_testcase"
+other_vars="include_deployment_testcase timeout_in_minutes"
 for var in $other_vars
 do
   integration_config=$(echo ${integration_config} | jq ".${var}=${!var}")
