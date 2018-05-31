819fe53303981bf37169a98ab729d7e980aa068d
Josh Hill <jhill@pivotal.io>
2018-05-31 12:05:00 +0100

    Remove TEAM_GPG_KEY from ci tasks
    The git-crypt feature of git-resource is now used to unlock meta repo.
    Default BOSH_CA_CERT_DIR to meta repo in bosh-delete-deployment task.
    
    [#157485442]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- tasks/bosh-delete-deployment/task.sh
+++ tasks/bosh-delete-deployment/task.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -eu
 
-if [[ ! -z "$BOSH_CA_CERT_PREPARE_CMD" ]]; then
-  "$BOSH_CA_CERT_DIR/$BOSH_CA_CERT_PREPARE_CMD"
-fi
-
-export BOSH_CLIENT
-export BOSH_CLIENT_SECRET
-export BOSH_ENVIRONMENT
 export BOSH_CA_CERT="$BOSH_CA_CERT_DIR/$BOSH_CA_CERT_PATH"
 
-bosh-cli delete-deployment -d ${BOSH_DEPLOYMENT} -n
-
-
+bosh-cli delete-deployment -d "${BOSH_DEPLOYMENT}" -n
