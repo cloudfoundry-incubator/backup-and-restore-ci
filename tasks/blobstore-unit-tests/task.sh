8328ec1e4800bb7721cda2cb959bd6fad20c1911
Chunyi Lyu <clyu@pivotal.io>
2018-05-31 11:27:05 +0100

    Run azure unit test in blobstore-unit-tests
    [#157994374]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- tasks/blobstore-unit-tests/task.sh
+++ tasks/blobstore-unit-tests/task.sh
@@ -18,14 +18,12 @@
 
 set -eu
 
-export TEST_AWS_ACCESS_KEY_ID
-export TEST_AWS_SECRET_ACCESS_KEY
-
-export TEST_ECS_ACCESS_KEY_ID
-export TEST_ECS_SECRET_ACCESS_KEY
-
 export GOPATH=`pwd`/backup-and-restore-sdk-release:"$GOPATH"
 
 pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore
   ginkgo -r -p -v -skipPackage=system_tests -keepGoing --flakeAttempts=2
+popd
+
+pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/azure-blobstore-backup-restore
+  ginkgo -r -p -v -skipPackage=system_tests -keepGoing
 popd
