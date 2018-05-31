2d8be719d65a8e234ab8f12f63bb7f0273a2958d
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-05-31 14:13:36 +0100

    Use git-crypt feature of git-resource to unlock meta repo
    [#157485442]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- tasks/terraform-operation/task.sh
+++ tasks/terraform-operation/task.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -eu
 
 terraform version
-
-if [[ ! -z "$TERRAFORM_STATE_PREPARE_CMD" ]]; then
-  "$TERRAFORM_STATE_DIR/$TERRAFORM_STATE_PREPARE_CMD"
-fi
 
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
 pushd "$TERRAFORM_STATE_DIR/$ENVIRONMENT_NAME"
   terraform init
-  terraform $TERRAFORM_OPERATION -auto-approve
+  terraform "$TERRAFORM_OPERATION" -auto-approve
 
   git add terraform.tfstate*
 
--- tasks/update-cloud-config/task.sh
+++ tasks/update-cloud-config/task.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-eval "$(bbl print-env --state-dir bbl-state/${BBL_STATE_DIR})"
+eval "$(bbl print-env --state-dir "bbl-state/${BBL_STATE_DIR}")"
 bosh cloud-config > cloud-config.yml
 bosh update-cloud-config -o "ops-files/${OPS_FILE_PATH}" -n cloud-config.yml
