7b13c2dfeb84378ae8e8a3ccc1d39685f5af833f
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-07-02 12:04:22 +0100

    Use a better name for the addon SDK release
    [#157603273]

--- tasks/create-addon-release/task.sh
+++ tasks/create-addon-release/task.sh
@@ -26,7 +26,7 @@ pushd backup-and-restore-sdk-release
 
   bosh-cli create-release \
     --version "${VERSION}" \
-    --name="backup-and-restore-sdk-preview" \
+    --name="backup-and-restore-sdk-addon" \
     --tarball="../backup-and-restore-sdk-release-build/backup-and-restore-sdk-addon-${VERSION}.tgz" \
     --force
 popd
