ab3c225e6ecfb926b0830a477f32e6143c2ba420
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-07-09 12:29:49 +0100

    move bosh related vars out from terraform dir
    [#157485287]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- tasks/update-b-drats-cloud-config/task.sh
+++ tasks/update-b-drats-cloud-config/task.sh
@@ -5,14 +5,15 @@ set -u
 
 pushd terraform-state
     bosh_host="$(terraform output director-ip)"
-    bosh_ca_cert="$(bosh-cli int --path=/director_ssl/ca "../bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
-    bosh_client_secret="$(bosh-cli int --path=/admin_password "../bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
     zone="$(terraform output zone1)"
     network="$(terraform output director-network-name)"
     subnetwork="$(terraform output director-subnetwork-name)"
     tags="[$(terraform output internal-tag)]"
     internal_cidr="$(terraform output director-subnetwork-cidr-range)"
 popd
+
+bosh_ca_cert="$(bosh-cli int --path=/director_ssl/ca "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
+bosh_client_secret="$(bosh-cli int --path=/admin_password "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
 
 
 bosh_ca_cert_path="$(mktemp)"
