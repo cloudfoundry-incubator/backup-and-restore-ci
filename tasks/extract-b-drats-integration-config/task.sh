0d0fcfef122227cd342b26f7d6c263e4bce1e260
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-07-09 15:06:40 +0100

    extract-b-drats-integration-config just consumes terraform-state
    - remove prepare commands, they are not longer needed
    
    [#157485287]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- tasks/extract-b-drats-integration-config/task.sh
+++ tasks/extract-b-drats-integration-config/task.sh
@@ -3,15 +3,10 @@
 set -e
 set -u
 
-if [[ ! -z "${TERRAFORM_STATE_PREPARE_CMD}" ]]; then
-  "${TERRAFORM_STATE_PREPARE_CMD}"
-fi
+pushd terraform-state
+    bosh_host="$(terraform output director-ip)"
+popd
 
-if [[ ! -z "${BOSH_VARS_STORE_PREPARE_CMD}" ]]; then
-  "${BOSH_VARS_STORE_PREPARE_CMD}"
-fi
-
-bosh_host="$(terraform output -state="${TERRAFORM_STATE_PATH}" director-ip)"
 bosh_ssh_username="${BOSH_SSH_USERNAME}"
 bosh_ssh_private_key="$(bosh-cli int --path=/jumpbox_ssh/private_key "${BOSH_VARS_STORE_PATH}")"
 timeout_in_minutes="${TIMEOUT_IN_MINUTES}"
