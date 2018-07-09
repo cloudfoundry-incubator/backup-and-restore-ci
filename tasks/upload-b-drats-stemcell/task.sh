5ae7873936f0eb2cc8921f2ce7a314c51867aa35
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-07-09 12:26:49 +0100

    upload-b-drats-stemcell just consumes terraform-state
    - remove prepare command since they are no longer needed
    
    [#157485287]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- tasks/upload-b-drats-stemcell/task.sh
+++ tasks/upload-b-drats-stemcell/task.sh
@@ -3,15 +3,10 @@
 set -e
 set -u
 
-if [[ ! -z "${TERRAFORM_STATE_PREPARE_CMD}" ]]; then
-  "terraform-state/${TERRAFORM_STATE_PREPARE_CMD}"
-fi
+pushd terraform-state
+    bosh_host="$(terraform output director-ip)"
+popd
 
-if [[ ! -z "${BOSH_VARS_STORE_PREPARE_CMD}" ]]; then
-  "bosh-vars-store/${BOSH_VARS_STORE_PREPARE_CMD}"
-fi
-
-bosh_host="$(terraform output -state="terraform-state/${TERRAFORM_STATE_PATH}" director-ip)"
 bosh_ca_cert="$(bosh-cli int --path=/director_ssl/ca "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
 bosh_client_secret="$(bosh-cli int --path=/admin_password "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
 
