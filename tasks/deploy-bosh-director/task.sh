fd46e9b1d3a6d1194810c307bba3d628e7f2383f
Mirah Gary <mgary@pivotal.io>
2018-08-09 09:37:43 +0100

    Use pushd and popd instead of cd in deploy-bosh-director task script
    [#159619865]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- tasks/deploy-bosh-director/task.sh
+++ tasks/deploy-bosh-director/task.sh
@@ -9,21 +9,21 @@ bosh-cli create-env \
   --var=test-release-path="file://${TEST_RELEASE_PATH}" \
   --vars-store="bosh-backup-and-restore-meta/${DIRECTOR_ENV}/creds.yml"
 
-cd bosh-backup-and-restore-meta
+pushd bosh-backup-and-restore-meta
+  git add "${DIRECTOR_ENV}/bosh-state.json"
 
-git add "${DIRECTOR_ENV}/bosh-state.json"
+  if [ -f "${DIRECTOR_ENV}/creds.yml" ]; then
+    git add "${DIRECTOR_ENV}/creds.yml"
+  fi
 
-if [ -f "${DIRECTOR_ENV}/creds.yml" ]; then
-  git add "${DIRECTOR_ENV}/creds.yml"
-fi
+  git config --global user.name "PCF Backup & Restore CI"
+  git config --global user.email "cf-lazarus@pivotal.io"
 
-git config --global user.name "PCF Backup & Restore CI"
-git config --global user.email "cf-lazarus@pivotal.io"
-
-if git commit -m "Update bosh state for ${DIRECTOR_ENV} director" ; then
-  echo "Updated bosh state for ${DIRECTOR_ENV} director"
-else
-  echo "No change in bosh state for ${DIRECTOR_ENV} director"
-fi
+  if git commit -m "Update bosh state for ${DIRECTOR_ENV} director" ; then
+    echo "Updated bosh state for ${DIRECTOR_ENV} director"
+  else
+    echo "No change in bosh state for ${DIRECTOR_ENV} director"
+  fi
+popd
 
 cp -r bosh-backup-and-restore-meta/. bosh-backup-and-restore-meta-updated/
