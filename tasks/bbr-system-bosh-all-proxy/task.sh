378242408912602f5aaef3120d48c724f4925452
Chunyi Lyu <clyu@pivotal.io>
2018-08-09 17:06:49 +0100

    Provide BOSH_ALL_PROXY port in only ci tasks
    [#159619865]

--- tasks/bbr-system-bosh-all-proxy/task.sh
+++ tasks/bbr-system-bosh-all-proxy/task.sh
@@ -10,7 +10,6 @@ ssh-add bosh-backup-and-restore-meta/keys/github
 echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
 chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
-export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
