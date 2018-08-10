1abda0803711e751469ec8fd1693d7b9fa7a3289
Jake Klein <jklein@pivotal.io>
2018-08-10 12:16:07 +0100

    Provide port for BOSH_ALL_PROXY in pipeline
    [#159619865]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- tasks/bbr-upload-system-test-releases/task.sh
+++ tasks/bbr-upload-system-test-releases/task.sh
@@ -6,7 +6,7 @@ if [[ "$USE_BOSH_ALL_PROXY" = true ]]; then
   echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
   chmod 0600 "${PWD}/ssh.key"
   export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
-  export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-key=${BOSH_GW_PRIVATE_KEY}"
+  export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 fi
 
 if [[ ! -z "$BOSH_CA_CERT_PATH" ]]; then
