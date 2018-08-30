d85d6bfc0d4bea14a8bbe8b545b88074c5d5bcf3
Josh Hill <jhill@pivotal.io>
2018-08-30 14:26:16 +0100

    Add extract-k-drats-config task
    Also extracted k-drats-master deployment name into a yaml variable.
    
    [#160089219]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

---
+++ tasks/extract-k-drats-config/task.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+pushd "bbl-state/$BBL_STATE_DIR"
+  eval "$(bbl print-env)"
+popd
+
+api_server_ip="$(jq -r .external_ip < terraform/metadata)"
+ca_cert="$(bosh int <(credhub get -n "${BOSH_DIRECTOR_NAME}/${BOSH_DEPLOYMENT}/tls-kubernetes" --output-json) --path=/value/ca)"
+password="$(bosh int <(credhub get -n "${BOSH_DIRECTOR_NAME}/${BOSH_DEPLOYMENT}/kubo-admin-password" --output-json) --path=/value)"
+
+integration_config="{}"
+integration_config=$(echo "$integration_config" | jq ".timeout_in_minutes=${TIMEOUT_IN_MINUTES}")
+integration_config=$(echo "$integration_config" | jq ".kubo.cluster_name=\"${CLUSTER_NAME}\"")
+integration_config=$(echo "$integration_config" | jq ".kubo.api_server_url=\"https://${api_server_ip}:8443\"")
+integration_config=$(echo "$integration_config" | jq ".kubo.ca_cert=\"${ca_cert}\"")
+integration_config=$(echo "$integration_config" | jq ".kubo.username=\"${KUBO_USERNAME}\"")
+integration_config=$(echo "$integration_config" | jq ".kubo.password=\"${password}\"")
+
+echo "$integration_config" > k-drats-config/config.json
