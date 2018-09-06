39750335f099a4722d5fecd6e9755067038f1c6b
Josh Hill <jhill@pivotal.io>
2018-09-06 14:36:39 +0100

    Use maru-lite for s3 system tests
    - group ec2 director creds in a var
    - use bosh vars to generate minio cert and key instead of generating
    them independently and passing in vars
    - Always use $BOSH_ALL_PROXY for s3 system tests
    - No need to add github ssh key to keychain in s3 system tests task
    
    [#160141487]
    
    Signed-off-by: Mirah Gary <mgary@pivotal.io>

--- tasks/s3-system-tests/task.sh
+++ tasks/s3-system-tests/task.sh
@@ -2,28 +2,19 @@
 
 set -eu
 
-eval "$(ssh-agent)"
-
-chmod 400 bosh-backup-and-restore-meta/keys/github
-ssh-add bosh-backup-and-restore-meta/keys/github
-
-export GOPATH="${PWD}/backup-and-restore-sdk-release"
-export PATH="$PATH:$GOPATH/bin"
-
-
 echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
 chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
 
+export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-key=${BOSH_GW_PRIVATE_KEY}"
 
-if [[ "$USE_BOSH_ALL_PROXY" = true ]]; then
-  export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-key=${BOSH_GW_PRIVATE_KEY}"
-fi
+export GOPATH="${PWD}/backup-and-restore-sdk-release"
+export PATH="${PATH}:${GOPATH}/bin"
 
-cd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/s3
-
-if [[ ! -z "${FOCUS_SPEC}" ]]; then
-  ginkgo -focus "${FOCUS_SPEC}" -v -r -trace
-else
-  ginkgo -v -r -trace
-fi
+pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/s3
+  if [[ ! -z "${FOCUS_SPEC}" ]]; then
+    ginkgo -focus "${FOCUS_SPEC}" -v -r -trace
+  else
+    ginkgo -v -r -trace
+  fi
+popd
