ae89fcbb996b85b5d08f2305712e701077a320fc
Jake Klein <jklein@pivotal.io>
2018-10-05 15:51:26 +0100

    Move GCS bloblstore sdk tests to maru lite
    [#160884947]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

---
+++ tasks/sdk-system-gcs/task.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+chmod 0600 "${PWD}/ssh.key"
+export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+
+export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-key=${BOSH_GW_PRIVATE_KEY}"
+
+export GOPATH="${PWD}/backup-and-restore-sdk-release"
+export PATH="${PATH}:${GOPATH}/bin"
+
+pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/gcs
+    ginkgo -v -r -trace
+popd
