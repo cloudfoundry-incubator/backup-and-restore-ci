58bbbacc6ff8529963af15eff53ac4e228d1152c
Mirah Gary <mgary@pivotal.io>
2018-10-19 11:59:04 +0100

    Generalize sdk blobstore system test task
    [#157838290]

--- tasks/sdk-system-blobstore/task.sh
+++ tasks/sdk-system-blobstore/task.sh
@@ -5,13 +5,12 @@ set -eu
 echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
 chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
-
 export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-key=${BOSH_GW_PRIVATE_KEY}"
 
 export GOPATH="${PWD}/backup-and-restore-sdk-release"
 export PATH="${PATH}:${GOPATH}/bin"
 
-pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/s3
+pushd "backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/${TEST_SUITE_NAME}"
   if [[ ! -z "${FOCUS_SPEC}" ]]; then
     ginkgo -focus "${FOCUS_SPEC}" -v -r -trace
   else
