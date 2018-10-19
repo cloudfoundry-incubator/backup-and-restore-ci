fe3fec397a1b1e08fa3004e5be07374ca1d4cdaf
Mirah Gary <mgary@pivotal.io>
2018-10-19 12:16:29 +0100

    Add gcs params to sdk-system-blobstore task
    Export gcp credential file in sdk-system-blobstore task script
    
    [#157838290]

--- tasks/sdk-system-blobstore/task.sh
+++ tasks/sdk-system-blobstore/task.sh
@@ -10,6 +10,10 @@ export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-k
 export GOPATH="${PWD}/backup-and-restore-sdk-release"
 export PATH="${PATH}:${GOPATH}/bin"
 
+GCP_SERVICE_ACCOUNT_KEY_PATH="$(mktemp)"
+echo "$GCP_SERVICE_ACCOUNT_KEY" > "$GCP_SERVICE_ACCOUNT_KEY_PATH"
+export GCP_SERVICE_ACCOUNT_KEY_PATH
+
 pushd "backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/${TEST_SUITE_NAME}"
   if [[ ! -z "${FOCUS_SPEC}" ]]; then
     ginkgo -focus "${FOCUS_SPEC}" -v -r -trace
