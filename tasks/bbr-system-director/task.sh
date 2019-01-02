2e47aab07aeb8fa0c080831411031701cec0f6e8
Josh Hill <jhill@pivotal.io>
2019-01-02 17:09:19 +0000

    Change bbr-system-director to use test-bosh-director
    [#162166637]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- tasks/bbr-system-director/task.sh
+++ tasks/bbr-system-director/task.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 set -eu
+set -o pipefail
 
+# Add GitHub SSH key to avoid rate-limit
 eval "$(ssh-agent)"
 chmod 400 bosh-backup-and-restore-meta/keys/github
-chmod 400 bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
 ssh-add bosh-backup-and-restore-meta/keys/github
 
-export BOSH_GW_HOST=$BOSH_ENVIRONMENT
+# Get BOSH Director SSH key from CredHub
+director_ssh_key="$(credhub find -n "$BOSH_DEPLOYMENT/jumpbox_ssh" --output-json \
+ | jq -r .credentials[0].name \
+ | xargs credhub get --output-json -n \
+ | jq -r .value.private_key)"
+
+DIRECTOR_SSH_KEY_PATH="$(mktemp)"
+echo -e "${director_ssh_key}" > "$DIRECTOR_SSH_KEY_PATH"
+chmod 0600 "$DIRECTOR_SSH_KEY_PATH"
+export DIRECTOR_SSH_KEY_PATH
+
+# Set BOSH_ALL_PROXY to run bbr through SSH tunnel
+jumpbox_private_key_path="$(mktemp)"
+echo -e "${JUMPBOX_PRIVATE_KEY}" > "$jumpbox_private_key_path"
+chmod 0600 "$jumpbox_private_key_path"
+
+export BOSH_ALL_PROXY="ssh+socks5://${JUMPBOX_USER}@${JUMPBOX_HOST}?private-key=${jumpbox_private_key_path}"
+
+# Set up GOPATH
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
-export BOSH_GW_USER=${BOSH_GW_USER:-vcap}
-export BOSH_GW_PRIVATE_KEY=$PWD/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
-export SSH_KEY=$PWD/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
-export BOSH_CA_CERT=$PWD/bosh-backup-and-restore-meta/certs/$BOSH_ENVIRONMENT.crt
 
 cd src/github.com/cloudfoundry-incubator/bosh-backup-and-restore
 make sys-test-director-ci
