e548e2ceef91dee5ca05b66e1e3abe8ffe23f463
Josh Hill <jhill@pivotal.io>
2019-01-03 18:23:40 +0000

    Update bbr-system-director task
    - Remove default param values
    - Build CREDHUB_PROXY in task
    - Extract test-bosh-director deployment variable
    
    [#162166637]

--- tasks/bbr-system-director/task.sh
+++ tasks/bbr-system-director/task.sh
@@ -8,6 +8,13 @@ eval "$(ssh-agent)"
 chmod 400 bosh-backup-and-restore-meta/keys/github
 ssh-add bosh-backup-and-restore-meta/keys/github
 
+# Write Jumpbox SSH key to file
+jumpbox_ssh_key_path="$(mktemp)"
+echo -e "${JUMPBOX_SSH_KEY}" > "$jumpbox_ssh_key_path"
+chmod 0600 "$jumpbox_ssh_key_path"
+
+export CREDHUB_PROXY="ssh+socks5://$JUMPBOX_USER@$JUMPBOX_HOST?private-key=$jumpbox_ssh_key_path"
+
 # Get BOSH Director SSH key from CredHub
 director_ssh_key="$(credhub find -n "$BOSH_DEPLOYMENT/jumpbox_ssh" --output-json \
  | jq -r .credentials[0].name \
@@ -18,11 +25,6 @@ DIRECTOR_SSH_KEY_PATH="$(mktemp)"
 echo -e "${director_ssh_key}" > "$DIRECTOR_SSH_KEY_PATH"
 chmod 0600 "$DIRECTOR_SSH_KEY_PATH"
 export DIRECTOR_SSH_KEY_PATH
-
-# Write Jumpbox SSH key to file
-jumpbox_ssh_key_path="$(mktemp)"
-echo -e "${JUMPBOX_SSH_KEY}" > "$jumpbox_ssh_key_path"
-chmod 0600 "$jumpbox_ssh_key_path"
 
 # Create tunnel to Director via Jumpbox
 ssh-add "$jumpbox_ssh_key_path"
