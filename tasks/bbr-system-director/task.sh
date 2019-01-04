75c5a922a9167464ac66a55bf45dddf1ba1dcddf
Josh Hill <jhill@pivotal.io>
2019-01-04 10:45:22 +0000

    Use privileged container to run sshuttle in bbr-system-director
    Fail when sshuttle does not start
    
    [#162166637]
    
    Signed-off-by: Mehul Modha <mmodha@pivotal.io>

--- tasks/bbr-system-director/task.sh
+++ tasks/bbr-system-director/task.sh
@@ -31,7 +31,13 @@ ssh-add "$jumpbox_ssh_key_path"
 sshuttle -r "${JUMPBOX_USER}@${JUMPBOX_HOST}" "$DIRECTOR_HOST/32" \
   --daemon \
   -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=600'
+echo "Establishing tunnel to Director via Jumpbox..."
 sleep 5
+
+if ! stat sshuttle.pid > /dev/null 2>&1; then
+  echo "Failed to start sshuttle daemon"
+  exit 1
+fi
 
 # Set up GOPATH
 export GOPATH=$PWD
