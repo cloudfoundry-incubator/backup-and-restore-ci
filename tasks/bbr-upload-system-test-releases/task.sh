4045e3d5b8a745681b6738a234bb1b2ab2364f73
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-01-17 15:41:54 +0000

    Deprecate support for genesis bosh in bbr-upload-system-test-releases task
    [#162166626]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- tasks/bbr-upload-system-test-releases/task.sh
+++ tasks/bbr-upload-system-test-releases/task.sh
@@ -2,16 +2,11 @@
 
 set -eu
 
-if [[ "$USE_BOSH_ALL_PROXY" = true ]]; then
-  echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
-  chmod 0600 "${PWD}/ssh.key"
-  export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
-  export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
-fi
+echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+chmod 0600 "${PWD}/ssh.key"
+export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
-if [[ ! -z "$BOSH_CA_CERT_PATH" ]]; then
-  export BOSH_CA_CERT="$PWD/$BOSH_CA_CERT_PATH"
-fi
 cd "bbr-deployment-test-releases/${RELEASE_NAME}"
 
 bosh-cli -n create-release --force
