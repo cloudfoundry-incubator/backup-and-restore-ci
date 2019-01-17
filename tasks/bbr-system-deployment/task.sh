946530096739f8fd97607e0540699300d198560a
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-01-17 15:48:51 +0000

    Remove BOSH_CA_CERT_PATH reference from bbr-system-deployment
    [#162166626]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- tasks/bbr-system-deployment/task.sh
+++ tasks/bbr-system-deployment/task.sh
@@ -7,10 +7,6 @@ chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
 export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
-if [[ -z "$BOSH_CA_CERT_PATH" ]]; then
-  export BOSH_CA_CERT="$PWD/$BOSH_CA_CERT_PATH"
-fi
-
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
 cd src/github.com/cloudfoundry-incubator/bosh-backup-and-restore
