cb663007cb935ec01588c68fe7af73db8b313d06
Chunyi Lyu <clyu@pivotal.io>
2019-01-17 15:37:21 +0000

    Update bbr pipeline&ci tasks to use maru-lite for sys deployment tests
    [#162166626]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- tasks/bbr-system-deployment/task.sh
+++ tasks/bbr-system-deployment/task.sh
@@ -2,21 +2,10 @@
 
 set -eu
 
-eval "$(ssh-agent)"
-chmod 400 bosh-backup-and-restore-meta/keys/github
-ssh-add bosh-backup-and-restore-meta/keys/github
-
-if [[ "$USE_BOSH_ALL_PROXY" = true ]]; then
-  echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
-  chmod 0600 "${PWD}/ssh.key"
-  export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
-  export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
-else
-  chmod 400 "${BOSH_GW_PRIVATE_KEY:-bosh-backup-and-restore-meta/genesis-bosh/bosh.pem}"
-  export BOSH_GW_HOST=$BOSH_ENVIRONMENT
-  export BOSH_GW_USER=${BOSH_GW_USER:-vcap}
-  export BOSH_GW_PRIVATE_KEY=$PWD/${BOSH_GW_PRIVATE_KEY:-bosh-backup-and-restore-meta/genesis-bosh/bosh.pem}
-fi
+echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+chmod 0600 "${PWD}/ssh.key"
+export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
 if [[ -z "$BOSH_CA_CERT_PATH" ]]; then
   export BOSH_CA_CERT="$PWD/$BOSH_CA_CERT_PATH"
