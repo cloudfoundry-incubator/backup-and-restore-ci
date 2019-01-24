0c380753c1d7824dc9071eb31fe13b95fc2c1930
Glen Rodgers <grodgers@pivotal.io>
2019-01-24 11:47:30 +0000

    Corrected repo name
    [#163327750]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- tasks/bbl-update-director-vm-type/task.sh
+++ tasks/bbl-update-director-vm-type/task.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-bosh_deployment="backup-and-restore-meta/maru-lite/bosh-deployment"
+bosh_deployment="bosh-backup-and-restore-meta/maru-lite/bosh-deployment"
 
-bosh interpolate -o "backup-and-restore-meta/$OPS_FILE_PATH" "$bosh_deployment/gcp/bosh-lite-vm-type.yml" > updated_ops_file
+bosh interpolate -o "bosh-backup-and-restore-meta/$OPS_FILE_PATH" "$bosh_deployment/gcp/bosh-lite-vm-type.yml" > updated_ops_file
 mv updated_ops_file "$bosh_deployment/gcp/bosh-lite-vm-type.yml"
 
 cp -r backup-and-restore-meta/ updated-meta
