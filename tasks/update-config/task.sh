13eee84be7806dc993959e3d8716d2b2154f5426
Josh Hill <jhill@pivotal.io>
2019-01-29 16:20:52 +0000

    Removed task to upload bosh-dns runtime-config
    [#163327750]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- tasks/update-config/task.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-eval "$(bbl print-env --state-dir "bbl-state/${BBL_STATE_DIR}")"
-bosh -n update-config --name "$NAME" --type "$TYPE" "bbl-state/${BBL_STATE_DIR}/${CONFIG_PATH}"
