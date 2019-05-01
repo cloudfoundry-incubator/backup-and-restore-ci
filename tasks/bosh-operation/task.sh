4314c802458cfa5359198628a9fc8b0f4be477fb
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-05-01 16:32:23 +0100

    Update bosh-operation with new b-drats setup
    We now expect a jumpbox to have been deployed to use for creating bosh
    
    [#163089547]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

--- tasks/bosh-operation/task.sh
+++ tasks/bosh-operation/task.sh
@@ -3,14 +3,16 @@
 set -e
 set -u
 
-opsfiles_arguments=""
-for op in ${OPS_FILES}
-  do
-    opsfiles_arguments="${opsfiles_arguments} -o ${op}"
-done
+gcp_version_account_key="$(mktemp)"
+echo "${GCP_SERVICE_ACCOUNT_KEY}" > "${gcp_version_account_key}"
+
+jumpbox_private_key="$(mktemp)"
+bosh interpolate --path /jumpbox_ssh/private_key "bosh-state/${JUMPBOX_ENVIRONMENT_NAME}/creds.yml" > "${jumpbox_private_key}"
 
 pushd terraform-state
-  external_ip=$(terraform output director-ip)
+  jumbox_ip="$(terraform output jumpbox-ip)"
+  internal_gw="$(terraform output internal-gw)"
+  internal_ip="$(terraform output director-internal-ip)"
   zone="$(terraform output zone1)"
   network="$(terraform output director-network-name)"
   subnetwork="$(terraform output director-subnetwork-name)"
@@ -20,8 +22,8 @@ pushd terraform-state
 popd
 
 function commit_bosh_state() {
-  pushd "${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}"
-    git add bosh-state.json
+  pushd "bosh-state/${ENVIRONMENT_NAME}"
+    git add state.json
     git add creds.yml
     if git commit -m "Update bosh state for ${ENVIRONMENT_NAME} after bosh ${BOSH_OPERATION}"; then
       echo "Updated bosh-state for ${ENVIRONMENT_NAME} after bosh ${BOSH_OPERATION}"
@@ -30,33 +32,37 @@ function commit_bosh_state() {
     fi
   popd
 
-  cp -r "${BOSH_STATE_DIR}/." "${BOSH_STATE_OUTPUT_DIR}"
+  cp -r "bosh-state/." "bosh-state-updated"
 }
 
 trap commit_bosh_state EXIT
 
-gcp_version_account_key="$(mktemp)"
-echo "${GCP_SERVICE_ACCOUNT_KEY}" > "${gcp_version_account_key}"
+
+export BOSH_ALL_PROXY="ssh+socks5://jumpbox@${jumbox_ip}:22?private-key=${jumpbox_private_key}"
 
 pushd bosh-deployment
     # shellcheck disable=SC2086   # we need to expand $opsfiles_arguments
     bosh ${BOSH_OPERATION} bosh.yml \
-    --state="../${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}/bosh-state.json" \
-    --vars-store="../${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}/creds.yml" \
+    --state="../bosh-state/${ENVIRONMENT_NAME}/state.json" \
+    --vars-store="../bosh-state/${ENVIRONMENT_NAME}/creds.yml" \
     --var-file gcp_credentials_json="${gcp_version_account_key}" \
-    $opsfiles_arguments \
+    -o gcp/cpi.yml \
+    -o uaa.yml \
+    -o credhub.yml \
+    -o jumpbox-user.yml \
+    -o bbr.yml \
+    -o "../opsfiles/opsfiles/b-drats/gcp/bosh-director-ephemeral-ip-ops.yml" \
     -v director_name="${DIRECTOR_NAME}" \
     -v internal_cidr="${internal_cidr}" \
-    -v internal_gw=10.0.0.1 \
-    -v internal_ip=10.0.0.6 \
+    -v internal_gw="${internal_gw}" \
+    -v internal_ip="${internal_ip}" \
     -v project_id="${project_id}" \
     -v zone="${zone}" \
     -v tags="${tags}" \
     -v network="${network}" \
-    -v external_ip="${external_ip}" \
     -v subnetwork="${subnetwork}"
 popd
 
 if [[ ${BOSH_OPERATION} == "delete-env" ]]; then
-    rm "${BOSH_STATE_DIR}/${ENVIRONMENT_NAME}/creds.yml"
+    rm "bosh-state/${ENVIRONMENT_NAME}/creds.yml"
 fi
