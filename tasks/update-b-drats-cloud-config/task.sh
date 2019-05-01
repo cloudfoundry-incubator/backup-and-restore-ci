aab149ed324611fc04a6aa2196bcdfe0ef47459b
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-05-01 17:00:58 +0100

    Update update-b-drats-cloud-config for new b-drats directors
    We've changed how we deploy our directors. Now we use bosh-all-proxy and
    need to reserver the jumpbox ip
    
    [#163089547]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- tasks/update-b-drats-cloud-config/task.sh
+++ tasks/update-b-drats-cloud-config/task.sh
@@ -3,8 +3,14 @@
 set -e
 set -u
 
+jumpbox_private_key="$(mktemp)"
+bosh interpolate --path /jumpbox_ssh/private_key "bosh-vars-store/${JUMPBOX_VARS_STORE_PATH}" > "${jumpbox_private_key}"
+
 pushd terraform-state
     bosh_host="$(terraform output director-ip)"
+    jumpbox_ip="$(terraform output jumpbox-ip)"
+    jumpbox_internal_ip="$(terraform output jumpbox-internal-ip)"
+    internal_gw="$(terraform output internal-gw)"
     zone="$(terraform output zone1)"
     network="$(terraform output director-network-name)"
     subnetwork="$(terraform output director-subnetwork-name)"
@@ -19,15 +25,19 @@ bosh_client_secret="$(bosh int --path=/admin_password "bosh-vars-store/${BOSH_VA
 bosh_ca_cert_path="$(mktemp)"
 echo "${bosh_ca_cert}" > "${bosh_ca_cert_path}"
 
+export BOSH_ALL_PROXY="ssh+socks5://jumpbox@${jumpbox_ip}:22?private-key=${jumpbox_private_key}"
+
 bosh --environment "${bosh_host}" \
-    --client "${BOSH_CLIENT}" \
-    --client-secret "${bosh_client_secret}" \
-    --ca-cert "${bosh_ca_cert_path}" \
-    update-cloud-config \
-    -v internal_cidr="${internal_cidr}" \
-    -v internal_gw=10.0.0.1 \
-    -v zone="${zone}" \
-    -v network="${network}" \
-    -v subnetwork="${subnetwork}" \
-    -v tags="${tags}" \
-    "cloud-config/${CLOUD_CONFIG_PATH}" -n
+  --client "${BOSH_CLIENT}" \
+  --client-secret "${bosh_client_secret}" \
+  --ca-cert "${bosh_ca_cert_path}" \
+  update-cloud-config \
+  -o "opsfiles/opsfiles/b-drats/gcp/cloud-config-jumpbox-reserved-ip.yml" \
+  -v internal_cidr="${internal_cidr}" \
+  -v internal_gw="${internal_gw}"\
+  -v zone="${zone}" \
+  -v network="${network}" \
+  -v subnetwork="${subnetwork}" \
+  -v tags="${tags}" \
+  -v subnetwork_reserved_ip="${jumpbox_internal_ip}" \
+  "cloud-config/${CLOUD_CONFIG_PATH}" -n
