65bbfde189045dd2d19f07f5445cff0e49b6e87b
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-05-02 09:56:17 +0100

    Update resurrection now uses the jumpbox
    [#163089547]

--- tasks/update-b-drats-resurrection/task.sh
+++ tasks/update-b-drats-resurrection/task.sh
@@ -3,8 +3,12 @@
 set -e
 set -u
 
+jumpbox_private_key="$(mktemp)"
+bosh interpolate --path /jumpbox_ssh/private_key "bosh-vars-store/${JUMPBOX_VARS_STORE_PATH}" > "${jumpbox_private_key}"
+
 pushd terraform-state
-    bosh_host="$(terraform output director-ip)"
+    jumpbox_ip="$(terraform output jumpbox-ip)"
+    bosh_host="$(terraform output director-internal-ip)"
 popd
 
 bosh_ca_cert="$(bosh int --path=/director_ssl/ca "bosh-vars-store/${BOSH_VARS_STORE_PATH}")"
@@ -13,8 +17,10 @@ bosh_client_secret="$(bosh int --path=/admin_password "bosh-vars-store/${BOSH_VA
 bosh_ca_cert_path="$(mktemp)"
 echo "${bosh_ca_cert}" > "${bosh_ca_cert_path}"
 
+export BOSH_ALL_PROXY="ssh+socks5://jumpbox@${jumpbox_ip}:22?private-key=${jumpbox_private_key}"
+
 bosh --environment "${bosh_host}" \
-    --client "${BOSH_CLIENT}" \
-    --client-secret "${bosh_client_secret}" \
-    --ca-cert "${bosh_ca_cert_path}" \
-    update-resurrection "${RESURRECTION}" -n
+  --client "${BOSH_CLIENT}" \
+  --client-secret "${bosh_client_secret}" \
+  --ca-cert "${bosh_ca_cert_path}" \
+  update-resurrection "${RESURRECTION}" -n
