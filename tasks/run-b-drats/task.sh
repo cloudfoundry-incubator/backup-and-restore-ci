88383a439defc47ef71fac3d26738e95efdd5254
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-05-02 14:54:03 +0100

    run-b-drats tasks consumes terraform-state
    [#163089547]

--- tasks/run-b-drats/task.sh
+++ tasks/run-b-drats/task.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+jumpbox_ip="$( terraform output -state terraform-state/terraform.tfstate jumpbox-ip )"
 jumpbox_private_key="$( mktemp )"
 bosh interpolate --path /jumpbox_ssh/private_key "bosh-vars-store/${JUMPBOX_VARS_STORE_PATH}" > "$jumpbox_private_key"
 
