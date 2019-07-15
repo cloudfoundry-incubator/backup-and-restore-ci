812354721412f532d2a3e2d01aad2b1f71ecfb8e
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-07-15 14:07:15 +0100

    Add enable-soft-delete-azure task
    [#167104034]
    
    Signed-off-by: Sarah Connor <sconnor@pivotal.io>

---
+++ tasks/enable-soft-delete-azure/task.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eux
+
+storage_account_name="$(terraform output -state=terraform-state/terraform.tfstate "azure-storage-account-name")"
+
+az login --service-principal --username "$APP_ID" --password "$PASSWORD" --tenant "$TENANT_ID"
+
+az storage blob service-properties delete-policy update \
+  --days-retained 1  --account-name "$storage_account_name" --enable true
+
