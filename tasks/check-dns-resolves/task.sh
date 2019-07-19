ba300ee0de54d90f8b412ac15da71761ec000f93
Jake Klein <jklein@pivotal.io>
2019-07-19 09:36:40 +0100

    WIP- check dns resolves
    [#163694575]
    
    Signed-off-by: Sarah Connor <sconnor@pivotal.io>

--- tasks/check-dns-resolves/task.sh
+++ tasks/check-dns-resolves/task.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
+nameserver="$(jq -r .resources[0].instances[0].attributes.name_servers[0] terraform/terraform.tfstate)"
+
+nameserver="$(grep -Po '(ns-cloud-[a-z])' <<< "$nameserver")"
+
+dig "$ADDRESS" | grep "$nameserver"
 
