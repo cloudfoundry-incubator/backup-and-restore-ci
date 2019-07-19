0a01137be7188a37d4ad9319aed2caedb41fe0e8
Sarah Connor <sconnor@pivotal.io>
2019-07-19 11:54:44 +0100

    task should fail when dns resolution fails
    [#163694575]
    
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

--- tasks/check-dns-resolves/task.sh
+++ tasks/check-dns-resolves/task.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 nameserver="$( jq -r '.resources[0].instances[0].attributes.name_servers[0]' terraform/terraform.tfstate )"
 nameserver="$( grep -Po '(ns-cloud-[a-z])' <<< "$nameserver" )"
 
-if ! dig "$ADDRESS" | grep "$nameserver"; then
+dns_resolution="$( dig "$ADDRESS" )"
+
+if ! grep "$nameserver" <<< "$dns_resolution"; then
   2>&1 echo "could not resolve ${ADDRESS} through gcp nameservers"
+  2>&1 echo "$dns_resolution"
+  exit 1
 fi
 
