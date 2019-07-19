fe54e2f1ab0a903e31e2184a5da5d312592c51e6
Tom Godkin <tgodkin@pivotal.io>
2019-07-19 11:48:02 +0100

    Output helpful message when DNS resolution fails
    [#163694575]
    
    Signed-off-by: Sarah Connor <sconnor@pivotal.io>

--- tasks/check-dns-resolves/task.sh
+++ tasks/check-dns-resolves/task.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 
-nameserver="$(jq -r .resources[0].instances[0].attributes.name_servers[0] terraform/terraform.tfstate)"
+nameserver="$( jq -r '.resources[0].instances[0].attributes.name_servers[0]' terraform/terraform.tfstate )"
+nameserver="$( grep -Po '(ns-cloud-[a-z])' <<< "$nameserver" )"
 
-nameserver="$(grep -Po '(ns-cloud-[a-z])' <<< "$nameserver")"
-
-dig "$ADDRESS" | grep "$nameserver"
+if ! dig "$ADDRESS" | grep "$nameserver"; then
+  2>&1 echo "could not resolve ${ADDRESS} through gcp nameservers"
+fi
 
