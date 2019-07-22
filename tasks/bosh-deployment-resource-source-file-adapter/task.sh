150b21483d199ef01b10a3317bc5d5d22e4cddf2
Tom Godkin <tgodkin@pivotal.io>
2019-07-22 17:01:09 +0100

    Improve yml generation
    [#167167735]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

--- tasks/bosh-deployment-resource-source-file-adapter/task.sh
+++ tasks/bosh-deployment-resource-source-file-adapter/task.sh
@@ -9,14 +9,15 @@ function get_ip_port() {
 
 eval "$( bbl --state-dir "bosh-backup-and-restore-meta/$BBL_STATE" print-env )"
 
-cat > source-file/source-file.json <<EOF
-{
-  "jumpbox_username": "jumpbox",
-  "jumpbox_ssh_key": "$( awk '{printf "%s\\n", $0}' "${JUMPBOX_PRIVATE_KEY}" )",
-  "jumpbox_url": "$( get_ip_port "$BOSH_ALL_PROXY" )",
-  "target": "${BOSH_ENVIRONMENT}",
-  "client": "${BOSH_CLIENT}",
-  "client_secret": "${BOSH_CLIENT_SECRET}",
-  "cert": "$( awk '{printf "%s\\n", $0}' <<< "${BOSH_CA_CERT}" )"
-}
+yq write <( cat <<EOF
+---
+jumpbox_username: jumpbox
+jumpbox_url: $( get_ip_port "$BOSH_ALL_PROXY" )
+target: ${BOSH_ENVIRONMENT}
+client: ${BOSH_CLIENT}
+client_secret: ${BOSH_CLIENT_SECRET}
 EOF
+) -- "cert" "$BOSH_CA_CERT" > source-file/source-file.yml
+
+yq write --inplace -- source-file/source-file.yml jumpbox_ssh_key "$( cat "$JUMPBOX_PRIVATE_KEY" )"
+
