e7608795d7ceaa561bc2e03fccc92e0232298444
Sarah Connor <sconnor@pivotal.io>
2019-08-12 11:40:46 +0100

    Move bbr unit task to go mod
    [#167133813]

--- tasks/bbr-unit/task.sh
+++ tasks/bbr-unit/task.sh
@@ -4,8 +4,5 @@ set -euo pipefail
 eval "$( ssh-agent )"
 ssh-add - <<< "$GITHUB_SSH_KEY"
 
-export GOPATH="$PWD"
-export PATH="${PATH}:${GOPATH}/bin"
-
-cd src/github.com/cloudfoundry-incubator/bosh-backup-and-restore
-make test-ci
+cd bosh-backup-and-restore
+make test
