eb359ec32717b150a93a891edc338e982fe98910
Jake Klein <jklein@pivotal.io>
2019-08-15 16:31:01 +0100

    Cache go packages for much speed in CI
    [#164110744]
    
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

--- tasks/bbr-unit/task.sh
+++ tasks/bbr-unit/task.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$( cd "$( dirname "$1" )" && pwd )/$( basename "$1" )"
+}
+
+export GOPATH; GOPATH="$( get_abs_filename "../${GOPATH}" )"
+
 eval "$( ssh-agent )"
 ssh-add - <<< "$GITHUB_SSH_KEY"
 
