0700b25ed05497d7965d01ceef765ed717396f24
Jake Klein <jklein@pivotal.io>
2019-08-15 14:18:11 +0100

    Use a smaller fake-director deployment for system-director tests
    [#164110744]

--- tasks/bbr-system-director/task.sh
+++ tasks/bbr-system-director/task.sh
@@ -13,16 +13,8 @@ jumpbox_ssh_key_path="$(mktemp)"
 echo -e "${JUMPBOX_SSH_KEY}" > "$jumpbox_ssh_key_path"
 chmod 0600 "$jumpbox_ssh_key_path"
 
-export CREDHUB_PROXY="ssh+socks5://$JUMPBOX_USER@$JUMPBOX_HOST?private-key=$jumpbox_ssh_key_path"
-
-# Get BOSH Director SSH key from CredHub
-director_ssh_key="$(credhub find -n "$BOSH_DEPLOYMENT/jumpbox_ssh" --output-json \
- | jq -r .credentials[0].name \
- | xargs credhub get --output-json -n \
- | jq -r .value.private_key)"
-
 DIRECTOR_SSH_KEY_PATH="$(mktemp)"
-echo -e "${director_ssh_key}" > "$DIRECTOR_SSH_KEY_PATH"
+echo -e "${DIRECTOR_SSH_KEY}" > "$DIRECTOR_SSH_KEY_PATH"
 chmod 0600 "$DIRECTOR_SSH_KEY_PATH"
 export DIRECTOR_SSH_KEY_PATH
 
