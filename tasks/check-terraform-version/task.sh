fab57994e0797ca883c513701157003647827101
Gareth Smith <gsmith@pivotal.io>
2019-12-05 15:02:56 +0000

    Fix weird pipefail in check-terraform-version task
    [#170120116]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- tasks/check-terraform-version/task.sh
+++ tasks/check-terraform-version/task.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 
 main() {
-	local upstream_version downstream_version semver_regex
+	local upstream_version terraform_version_output downstream_version semver_regex
 	upstream_version="$( cat terraform-github-release/version )"
-	downstream_version="$( terraform -version | head -n 1 | awk '{ print $2 }' | sed -e 's/v//')"
+	# terraform exits with 141 if it can't print its *entire* output to stdout
+	terraform_version_output="$(terraform -version)"
+	# head closes stdin once it's gotten all the lines it wants
+	downstream_version="$( echo "${terraform_version_output}" | head -n 1 | awk '{ print $2 }' | sed -e 's/v//')"
 	semver_regex='^([0-9]+\.[0-9]+)\.([0-9]+)$'
 
 	echo "Comparing Upstream and Downstream Terraform versions..."
@@ -33,7 +36,7 @@ main() {
 		exit 1
 	fi
 
-	if [[ "${upstream_major_minor}" == "${downstream_major_minor}" ]]; then
+	if [[ "${upstream_patch}" == "${downstream_patch}" ]]; then
 		echo "Downstream version '${downstream_version}' is up-to-date!"
 	elif [[ "${upstream_patch}" -gt "${downstream_patch}" ]]; then
 		echo "Bumping to new patch version '${upstream_version}'!"
