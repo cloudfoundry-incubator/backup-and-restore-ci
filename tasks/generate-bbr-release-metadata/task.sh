807b3b0df8738fee9fd11ec43405db4924f75b4e
James Norman <jnorman@pivotal.io>
2020-09-21 14:39:32 +0000

    Include validator in github release
    [#174188111]
    
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- tasks/generate-bbr-release-metadata/task.sh
+++ tasks/generate-bbr-release-metadata/task.sh
@@ -24,6 +24,10 @@ create_release_for "$linux_binary"
 darwin_binary="bbr-${VERSION}-darwin-amd64"
 create_release_for "$darwin_binary"
 
+#Add BBR s3 Validator
+cp "release/README.md" $github_folder
+cp "release/bbr-s3-config-validator-"* $github_folder
+
 export LINUX_BINARY="${pivnet_folder}/${linux_binary}"
 export DARWIN_BINARY="${pivnet_folder}/${darwin_binary}"
 export RELEASE_TAR="${pivnet_folder}/${release_tar}"
