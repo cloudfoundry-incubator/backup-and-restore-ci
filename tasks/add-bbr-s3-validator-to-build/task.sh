872951efde528f1d1d83f76cb751b752834923a4
James Norman <jnorman@pivotal.io>
2020-09-22 11:30:43 +0000

    Include arch and os in s3-validator binary and hash
    [#174188111]
    
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- tasks/add-bbr-s3-validator-to-build/task.sh
+++ tasks/add-bbr-s3-validator-to-build/task.sh
@@ -10,6 +10,6 @@ pushd "bbr-s3-config-validator-artifact"
   tar -xf ./*.tgz
 
   cp README.md "$bbr_build/bbr-s3-config-validator-$bbr_version.README.md"
-  cp bbr-s3-config-validator "$bbr_build/bbr-s3-config-validator-$bbr_version"
-  cp bbr-s3-config-validator.sha256 "$bbr_build/bbr-s3-config-validator-$bbr_version.sha256"
+  cp bbr-s3-config-validator "$bbr_build/bbr-s3-config-validator-$bbr_version-linux-amd64"
+  cp bbr-s3-config-validator.sha256 "$bbr_build/bbr-s3-config-validator-$bbr_version-linux-amd64.sha256"
 popd
