d893cb7e1dce84da464ecf981a724fde8b9913be
Max Brauer <mbrauer@vmware.com>
2020-09-22 11:01:09 +0000

    Copies all required s3-validator files to build
    [#174188111]
    
    Signed-off-by: James Norman <jnorman@pivotal.io>

--- tasks/add-bbr-s3-validator-to-build/task.sh
+++ tasks/add-bbr-s3-validator-to-build/task.sh
@@ -6,6 +6,8 @@ bbr_build="$PWD/bbr-build"
 
 pushd "bbr-s3-config-validator-artifact"
   tar -xf *.tgz
-  cp README.md $bbr_build
-  cp bbr-s3-config-validator-* $bbr_build
+
+  cp README.md $bbr_build/bbr-s3-config-validator.README.md
+  cp bbr-s3-config-validator $bbr_build/bbr-s3-config-validator
+  cp bbr-s3-config-validator.sha256 $bbr_build/bbr-s3-config-validator.sha256
 popd
