cf91f739129cb83a72fb0f525687bc4d808eeb15
James Norman <jnorman@pivotal.io>
2020-09-23 08:35:19 +0000

    Pivnet release.yml now contains s3-validator
    [#174188111]
    
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- tasks/generate-bbr-release-metadata/task.sh
+++ tasks/generate-bbr-release-metadata/task.sh
@@ -24,10 +24,14 @@ create_release_for "$linux_binary"
 darwin_binary="bbr-${VERSION}-darwin-amd64"
 create_release_for "$darwin_binary"
 
-cp "release/bbr-s3-config-validator"* $github_folder
+cp "release/bbr-s3-config-validator"* $github_folder/
+cp "release/bbr-s3-config-validator-${VERSION}-linux-amd64" $pivnet_folder/
+cp "release/bbr-s3-config-validator-${VERSION}.README.md" $pivnet_folder/
 
-export LINUX_BINARY="${pivnet_folder}/${linux_binary}"
-export DARWIN_BINARY="${pivnet_folder}/${darwin_binary}"
+export BBR_LINUX_BINARY="${pivnet_folder}/${linux_binary}"
+export BBR_DARWIN_BINARY="${pivnet_folder}/${darwin_binary}"
 export RELEASE_TAR="${pivnet_folder}/${release_tar}"
+export BBR_S3_VALIDATOR_BINARY="${pivnet_folder}/bbr-s3-config-validator-${VERSION}-linux-amd64"
+export BBR_S3_VALIDATOR_README="${pivnet_folder}/bbr-s3-config-validator-${VERSION}.README.md"
 
 erb -T- "template-folder/${TEMPLATE_PATH}" > $pivnet_folder/release.yml
