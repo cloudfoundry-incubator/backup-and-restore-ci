ba1da719fd23d153afcf930ba05e8c94510c2de6
Gareth Smith <gsmith@pivotal.io>
2020-09-30 08:34:01 +0000

    Add logging to bbr-build task
    [#174987088]

--- tasks/bbr-build/task.sh
+++ tasks/bbr-build/task.sh
@@ -14,6 +14,8 @@ pushd "$BBR_REPO"
   make release
 popd
 
+echo "BBR successfully built. Copying to release directory..."
+
 cp -r "$BBR_REPO/releases" bbr-release
 
 echo "Auto-delivered in
@@ -21,12 +23,16 @@ https://s3-eu-west-1.amazonaws.com/bosh-backup-and-restore-builds/bbr-$VERSION.t
 
 [Backup and Restore Bot]" > bbr-build/message
 
+echo "Moving linux binary to the build directory..."
+
 LINUX="bbr-$VERSION-linux-amd64"
 mv "$BBR_REPO"/releases/bbr bbr-build/"$LINUX"
 cat "$BBR_REPO"/releases/checksum.sha256 | cut -d' ' -f1  | sed -n '1p' > bbr-build/"$LINUX".sha256
+
+echo "Moving mac binary to the build directory..."
 
 DARWIN="bbr-$VERSION-darwin-amd64"
 mv "$BBR_REPO"/releases/bbr-mac bbr-build/"$DARWIN"
 cat "$BBR_REPO"/releases/checksum.sha256 | cut -d' ' -f1  | sed -n '2p' > bbr-build/"$DARWIN".sha256
 
-
+echo "Done building BBR"
