7d269dd8c211fff1f04de209033f72f61ab28573
James Norman <jnorman@pivotal.io>
2020-10-09 13:28:14 +0000

    Task now creates tunnel via soho to external db
    [#174188111]

--- tasks/create-external-cf-databases/task.sh
+++ tasks/create-external-cf-databases/task.sh
@@ -9,8 +9,19 @@ db_password="$(grep "external\\_.*\\_database\\_password" "vars-store/$VARS_STOR
 
 databases="$(grep "external\\_.*\\_database\\_name" "vars-store/$VARS_STORE_FILE" | awk '{print $2}')"
 
+#Create tunnel to MYSQL instance via soho
+jumpbox_key="$(mktmp)"
+echo -e $JUMPBOX_KEY > $jumpbox_key
+chmod 0600 $jumpbox_key
+eval `ssh-agent`
+ssh-add  $jumpbox_key
+
+ssh "3306:$db_endpoint:3306 $JUMPBOX -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no &"
+
+sleep 5
+
 for db_name in $databases; do
   echo "Creating $db_name"
-  mysql -h"$db_endpoint" -u"$db_username" -p"$db_password" -e "create database if not exists \`$db_name\`;"
+  MYSQL_PWD="$db_password" mysql -h 127.0.0.1 -u"$db_username" -e "create database if not exists \`$db_name\`;"
   sleep 0.1
 done
