89eb2be8a5e9ad10e193546f2aa52dfbe7e22d36
James Norman <jnorman@pivotal.io>
2020-10-13 13:39:17 +0100

    Revert SSH tunnel work
    Revert "Amend typo jumpbox now params not input_mapping"
    
    This reverts commit 475cb44e8d44dd0d4ad0147df3303a7fe55aa197.
    
    Revert "Add SSH tunnel to RDS database teardown"
    
    This reverts commit d9cc43b9d71551569203642169a4cb797183436f.
    
    Revert "Quote key variable to avoid newline issue"
    
    This reverts commit bda9a6b143440eb69474dd9009410cb0ddcb550f.
    
    Revert "Amend typo on mktemp"
    
    This reverts commit 1f421a3dfaa4494233c8121390402cc748a60855.
    
    Revert "Parameterise JUMPBOX input for SDK pipeline"
    
    This reverts commit 424fa75ec4f53db47ac3345dde82fce31bf47f0c.
    
    Revert "Task now creates tunnel via soho to external db"
    
    This reverts commit 7d269dd8c211fff1f04de209033f72f61ab28573.

--- tasks/create-external-cf-databases/task.sh
+++ tasks/create-external-cf-databases/task.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefail
+
+set -e
+set -u
 
 db_endpoint="$(grep "external\\_.*\\_database\\_address" "vars-store/$VARS_STORE_FILE" -m1 | awk '{print $2}')"
 db_username="$(grep "external\\_.*\\_database\\_username" "vars-store/$VARS_STORE_FILE" -m1 | awk '{print $2}')"
@@ -7,19 +9,8 @@ db_password="$(grep "external\\_.*\\_database\\_password" "vars-store/$VARS_STOR
 
 databases="$(grep "external\\_.*\\_database\\_name" "vars-store/$VARS_STORE_FILE" | awk '{print $2}')"
 
-#Create tunnel to MYSQL instance via soho
-jumpbox_key="$(mktemp)"
-echo -e "$JUMPBOX_KEY" > $jumpbox_key
-chmod 0600 $jumpbox_key
-eval `ssh-agent`
-ssh-add  $jumpbox_key
-
-ssh -L "3306:$db_endpoint:3306" "$JUMPBOX" -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no &
-
-sleep 5
-
 for db_name in $databases; do
   echo "Creating $db_name"
-  MYSQL_PWD="$db_password" mysql --host 127.0.0.1 --user "$db_username" --execute "create database if not exists \`$db_name\`;"
+  mysql -h"$db_endpoint" -u"$db_username" -p"$db_password" -e "create database if not exists \`$db_name\`;"
   sleep 0.1
 done
