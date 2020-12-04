093de9e8e0f2186293670e27222eca9e7f3f73fd
Neil Hickey <nhickey@pivotal.io>
2020-12-04 23:26:39 +0000

    Add proxy to director tests
    [#175938289]

--- tasks/bbr-system-director/task.sh
+++ tasks/bbr-system-director/task.sh
@@ -31,5 +31,10 @@ if ! stat sshuttle.pid > /dev/null 2>&1; then
   exit 1
 fi
 
+echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+chmod 0600 "${PWD}/ssh.key"
+export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
+
 cd bosh-backup-and-restore
 make sys-test-director-ci
