8e58a9c7889d2797e504973e24769f0b5befc84f
Neil Hickey <nhickey@pivotal.io>
2020-12-10 22:56:23 +0000

    Simplify bbr-system-director test
    - remove duplication of jumpbox params
    
    [#175938096]

--- tasks/bbr-system-director/task.sh
+++ tasks/bbr-system-director/task.sh
@@ -8,10 +8,10 @@ eval "$(ssh-agent)"
 chmod 400 bosh-backup-and-restore-meta/keys/github
 ssh-add bosh-backup-and-restore-meta/keys/github
 
-# Write Jumpbox SSH key to file
-jumpbox_ssh_key_path="$(mktemp)"
-echo -e "${JUMPBOX_SSH_KEY}" > "$jumpbox_ssh_key_path"
-chmod 0600 "$jumpbox_ssh_key_path"
+# # Write Jumpbox SSH key to file
+echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+chmod 0600 "${PWD}/ssh.key"
+export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
 
 DIRECTOR_SSH_KEY_PATH="$(mktemp)"
 echo -e "${DIRECTOR_SSH_KEY}" > "$DIRECTOR_SSH_KEY_PATH"
@@ -19,8 +19,8 @@ chmod 0600 "$DIRECTOR_SSH_KEY_PATH"
 export DIRECTOR_SSH_KEY_PATH
 
 # Create tunnel to Director via Jumpbox
-ssh-add "$jumpbox_ssh_key_path"
-sshuttle -r "${JUMPBOX_USER}@${JUMPBOX_HOST}" "$DIRECTOR_HOST/32" \
+ssh-add "$BOSH_GW_PRIVATE_KEY"
+sshuttle -r "${BOSH_GW_USER}@${BOSH_GW_HOST}" "$DIRECTOR_HOST/32" \
   --daemon \
   -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=600'
 echo "Establishing tunnel to Director via Jumpbox..."
@@ -31,9 +31,6 @@ if ! stat sshuttle.pid > /dev/null 2>&1; then
   exit 1
 fi
 
-echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
-chmod 0600 "${PWD}/ssh.key"
-export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
 export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
 cd bosh-backup-and-restore
