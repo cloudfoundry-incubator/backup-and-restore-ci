d8925cc205e6015020a4364b480dddcc1b980ffe
Fernando Naranjo <fnaranjo@vmware.com>
2020-12-15 17:55:00 +0000

    Terraform v0.14.2 output command breaks the script
    Stripping quotes from terraform output fixes the issue
    [#176076305]
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- tasks/enable-soft-delete-azure/task.sh
+++ tasks/enable-soft-delete-azure/task.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-storage_account_name="$(terraform output -state=terraform-state/terraform.tfstate "azure-storage-account-name")"
+storage_account_name="$(terraform output -state=terraform-state/terraform.tfstate "azure-storage-account-name" | jq -r .)"
 
 az login --service-principal --username "$APP_ID" --password "$PASSWORD" --tenant "$TENANT_ID"
 
