8a1c447e7a4f7d507da9782739aea30f758802de
Neil Hickey <nhickey@pivotal.io>
2021-01-12 23:35:18 +0000

    Publish all binaries in the 'release' folder
    [#176383024]

--- tasks/generate-bbr-release-metadata/task.sh
+++ tasks/generate-bbr-release-metadata/task.sh
@@ -2,38 +2,26 @@
 
 set -euo pipefail
 
-pivnet_folder="pivnet-release-with-metadata"
-github_folder="github-release-with-metadata"
-
-function create_release_for {
-  cp release/"$1"* $github_folder/
-  cp release/"$1" $pivnet_folder/
-}
+PIVNET_FOLDER="pivnet-release-with-metadata"
+GITHUB_FOLDER="github-release-with-metadata"
 
 version=$(cat "version-folder/${VERSION_PATH}")
 export VERSION="$version"
 
 echo "Creating release tarball..."
 release_tar="bbr-${VERSION}.tar"
-tar -cf "${release_tar}" release
+tar  -cf "${release_tar}" release
 
-cp "${release_tar}" $github_folder/
-cp "${release_tar}" $pivnet_folder/
+cp -r "release/." $github_folder/
+cp -r "release/." $pivnet_folder/
 
-linux_binary="bbr-${VERSION}-linux-amd64"
-create_release_for "$linux_binary"
+LINUX_BINARY="bbr-${VERSION}-linux-amd64"
+DARWIN_BINARY="bbr-${VERSION}-darwin-amd64"
 
-darwin_binary="bbr-${VERSION}-darwin-amd64"
-create_release_for "$darwin_binary"
-
-cp "release/bbr-s3-config-validator"* $github_folder/
-cp "release/bbr-s3-config-validator-${VERSION}-linux-amd64" $pivnet_folder/
-cp "release/bbr-s3-config-validator-${VERSION}.README.md" $pivnet_folder/
-
-export BBR_LINUX_BINARY="${pivnet_folder}/${linux_binary}"
-export BBR_DARWIN_BINARY="${pivnet_folder}/${darwin_binary}"
-export RELEASE_TAR="${pivnet_folder}/${release_tar}"
-export BBR_S3_VALIDATOR_BINARY="${pivnet_folder}/bbr-s3-config-validator-${VERSION}-linux-amd64"
-export BBR_S3_VALIDATOR_README="${pivnet_folder}/bbr-s3-config-validator-${VERSION}.README.md"
+export BBR_LINUX_BINARY="${PIVNET_FOLDER}/${LINUX_BINARY}"
+export BBR_DARWIN_BINARY="${PIVNET_FOLDER}/${DARWIN_BINARY}"
+export RELEASE_TAR="${PIVNET_FOLDER}/${release_tar}"
+export BBR_S3_VALIDATOR_BINARY="${PIVNET_FOLDER}/bbr-s3-config-validator-${VERSION}-linux-amd64"
+export BBR_S3_VALIDATOR_README="${PIVNET_FOLDER}/bbr-s3-config-validator-${VERSION}.README.md"
 
 erb -T- "template-folder/${TEMPLATE_PATH}" > $pivnet_folder/release.yml
