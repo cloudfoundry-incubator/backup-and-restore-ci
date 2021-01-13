7b60b76d22a276c2e059a8ca975307333575995c
Fernando Naranjo <fnaranjo@vmware.com>
2021-01-13 12:12:24 +0000

    Improved release-notes generation for automation
    - removed stories from template
    
    [#176394520]
    
    Signed-off-by: James Norman <normanja@vmware.com>

--- tasks/generate-release-notes/task.sh
+++ tasks/generate-release-notes/task.sh
@@ -3,11 +3,8 @@
 set -eux -o pipefail
 
 pushd repo
-  # find latest tag
   latest_tag=$(git describe --abbrev=0 --tags)
-  # git lg from that tag to current
-  export COMMITS=$(git log ${latest_tag}..HEAD  --format=' %n%B'  | grep -v "Signed-off-by" | grep -v '^$')
-  export STORIES=$(echo -e "$COMMITS" | grep "\[\#" | sort | uniq | awk -F'[^0-9]*' '{print $2}' | xargs -IN echo https://www.pivotaltracker.com/story/show/N)
+  export COMMITS=$(git log ${latest_tag}..HEAD --oneline | grep -v "Merge" | cut -d ' ' -f2-| uniq -u)
 popd
 
 erb -T- template-folder/${TEMPLATE_PATH} > release-notes/release-notes.md
