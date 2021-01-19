cbef100619ee446d65b0888b04637211188f3114
Neil Hickey <nhickey@vmware.com>
2021-01-19 10:26:51 -0700

    Add generate-docs job
    + generate docs in the current BBR docs format

---
+++ tasks/generate-docs/task.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ROOT_DIR="$PWD"
+VERSION=$(cat "version-folder/${VERSION_PATH}")
+
+pushd repo
+  latest_tag=$(git describe --abbrev=0 --tags)
+  export COMMITS=$(git log ${latest_tag}..HEAD --oneline | grep -v "Merge" | cut -d ' ' -f2-| uniq -u)
+popd
+
+erb -T- template-folder/${TEMPLATE_PATH} > "${ROOT_DIR}/generated-release-notes.txt"
+
+pushd docs-repo
+  git checkout -b "release-${VERSION}"
+
+  sed "/Releases r ${ROOT_DIR}/release-notes.txt" bbr-rn.html.md.erb > bbr-rn.html.md.erb
+  echo -e "\n > Generated Release Notes:"
+  cat bbr-rn.html.md.erb
+
+  git add bbr-rn.html.md.erb
+  git config --global user.name "Cryogenics CI"
+  git config --global user.email "cf-lazarus@pivotal.io"
+  git commit -m "Release Notes for version: ${VERSION}"
+popd
+
+cp -r docs-repo/. updated-docs-repo/
