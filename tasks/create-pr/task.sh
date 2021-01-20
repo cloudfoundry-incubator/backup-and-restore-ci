b0d36213e6c4ef485630e329249b52ee7540add4
Neil Hickey <nhickey@vmware.com>
2021-01-20 09:07:22 -0700

    Remove temp workaround for gh cli
    [#176383388]

--- tasks/create-pr/task.sh
+++ tasks/create-pr/task.sh
@@ -2,13 +2,6 @@
 
 set -euo pipefail
 
-apt update
-apt-get install -y software-properties-common
-apt-key adv --keyserver keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
-apt-add-repository https://cli.github.com/packages
-apt update
-apt install -y gh
-
 pushd repo
   gh auth login --with-token < <(echo $GITHUB_TOKEN)
   gh pr create --fill --base $BASE
