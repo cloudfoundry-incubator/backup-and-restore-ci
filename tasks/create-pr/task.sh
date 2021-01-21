c1d967881c6960da8c5a2773e5fac5d041f5f490
Neil Hickey <nhickey@vmware.com>
2021-01-21 09:47:20 -0700

    Add message to Pull Request
    + Checkout the branch prior to PR. Concourse leaves the repo in a
      "detached" state which cannot be PR'd from
    
    [#176383388]

--- tasks/create-pr/task.sh
+++ tasks/create-pr/task.sh
@@ -2,7 +2,17 @@
 
 set -euo pipefail
 
+read -r -d '' MESSAGE << EOM
+This is an automatically generated Pull Request from the Cryogenics CI Bot.
+
+I have updated the release notes with the latest release and it's contents.
+
+If this doesn't look right, please reach out to the #mapbu-cryogenics team.
+EOM
+
 pushd repo
+  git checkout $BRANCH
+
   gh auth login --with-token < <(echo $GITHUB_TOKEN)
-  gh pr create --fill --base $BASE
+  gh pr create --base $BASE --title "$MESSAGE"
 popd
