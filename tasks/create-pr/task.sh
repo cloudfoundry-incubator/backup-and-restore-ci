9fb7ada1be480687c51349cee380e2a7a23ef304
Neil Hickey <nhickey@vmware.com>
2021-01-21 10:00:13 -0700

    Fixup the message of a PR
    [#176383388]

--- tasks/create-pr/task.sh
+++ tasks/create-pr/task.sh
@@ -2,17 +2,18 @@
 
 set -euo pipefail
 
-read -r -d '' MESSAGE << EOM
+MESSAGE=$(cat <<-EOM
 This is an automatically generated Pull Request from the Cryogenics CI Bot.
 
-I have updated the release notes with the latest release and it's contents.
+I have updated the release notes with the latest release and contents.
 
-If this doesn't look right, please reach out to the #mapbu-cryogenics team.
+If this does not look right, please reach out to the #mapbu-cryogenics team.
 EOM
+)
 
 pushd repo
   git checkout $BRANCH
 
   gh auth login --with-token < <(echo $GITHUB_TOKEN)
-  gh pr create --base $BASE --title "$MESSAGE"
+  gh pr create --base $BASE --title "[CI Bot] Latest Release Notes" --body "$MESSAGE"
 popd
