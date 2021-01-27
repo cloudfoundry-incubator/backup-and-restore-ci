dcf66104753cb05367e4b782eb38285ae36e8130
Fernando Naranjo <fnaranjo@vmware.com>
2021-01-27 13:01:10 +0000

    Manage vendor updates using a different branch
    - Notify on new versions of golang-release
    - Allow task to push updates to different branch
    - Make pipeline push updates to different branch
    
    [#175938124]

--- tasks/bosh-vendor-package/task.sh
+++ tasks/bosh-vendor-package/task.sh
@@ -14,6 +14,15 @@ blobstore:
   bosh vendor-package "${VENDORED_PACKAGE_NAME}" ../vendored-package-release
 
   git add .
+  
+  if [ -z "$VENDOR_UPDATES_BRANCH" ]
+  then
+        curr_branch=$(git rev-parse --abbrev-ref HEAD)
+        echo "Pushing vendor updates to the same branch '${curr_branch}'"
+  else
+        git checkout -b "${VENDOR_UPDATES_BRANCH}"
+        echo "Pushing vendor updates to the configured branch '${VENDOR_UPDATES_BRANCH}'"
+  fi
 
   if git commit -m "Update vendored package ${VENDORED_PACKAGE_NAME}"; then
     echo "Updated vendored package ${VENDORED_PACKAGE_NAME}"
