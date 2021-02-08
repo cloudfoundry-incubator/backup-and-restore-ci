ee4eb9c55cb272908f6b1343d72bf970b3a7a77f
Fernando Naranjo <fnaranjo@vmware.com>
2021-02-08 12:43:04 +0000

    Allow customizing PR title and message
    [#176680335]
    
    Retaining backward compatibility.
    It checks if the variables have been set
    and fallbacks to previous existing TITLE
    and/or MESSAGE if missing.

--- tasks/create-pr/task.sh
+++ tasks/create-pr/task.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+if [ -z "${TITLE}"]
+then
+  TITLE="[CI Bot] Latest Release Notes"
+fi
+
+if [ -z "${MESSAGE}" ]
+then
 MESSAGE=$(cat <<-EOM
 This is an automatically generated Pull Request from the Cryogenics CI Bot.
 
@@ -10,10 +17,11 @@ I have updated the release notes with the latest release and contents.
 If this does not look right, please reach out to the #mapbu-cryogenics team.
 EOM
 )
+fi
 
 pushd repo
   git checkout $BRANCH
 
   gh auth login --with-token < <(echo $GITHUB_TOKEN)
-  gh pr create --base $BASE --title "[CI Bot] Latest Release Notes" --body "$MESSAGE"
+  gh pr create --base $BASE --title "${TITLE}" --body "$MESSAGE"
 popd
