622307f70f6328e9c42e219ab2fe7c3b34709b1a
Neil Hickey <nhickey@pivotal.io>
2021-02-22 15:58:20 -0700

    Ruthlessly delete tasks that are used by no pipelines
    (according to 'ag')
    
    - The following repos were checked:
    
    - backup-and-restore-sdk-release
    - bbr-pcf-pipeline-tasks
    - bosh-backup-and-restore
    - bosh-backup-and-restore-meta
    - bosh-disaster-recovery-acceptance-tests
    - pivotal-bosh-disaster-recovery-acceptance-tests
    - disaster-recovery-acceptance-tests
    - pivotal-disaster-recovery-acceptance-tests
    - cryogenics-meta
    
    [#176553853]
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- tasks/bosh-delete-deployment/task.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-export BOSH_CA_CERT="$BOSH_CA_CERT_DIR/$BOSH_CA_CERT_PATH"
-
-bosh delete-deployment -d "${BOSH_DEPLOYMENT}" -n
--- tasks/tracker-bot-config/task.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cp bosh-backup-and-restore-meta/tracker-bot/config.yml relint-trackerbot/
-mkdir -p relint-trackerbot-with-config/
-cp -r relint-trackerbot/. relint-trackerbot-with-config
